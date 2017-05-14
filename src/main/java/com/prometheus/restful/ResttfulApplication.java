baba3aa78ab2ec8d61cec75a16b409d780c09113
Antonio Tagliente <atagliente@new-host.station>
2017-05-14 13:25:03 +0200

    First test

---
+++ src/main/java/com/prometheus/restful/ResttfulApplication.java
@@ -0,0 +1,12 @@
+package com.prometheus.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttfulApplication.class, args);
+	}
+}
